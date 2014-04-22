a435fc759ac837461db6f681e2119d1c965faed3
Patrick Madden <maddenpj@gmail.com>
2014-04-22 17:14:04 -0500

    use C++ threads so that periodic executor doesn't block

--- expiration_test.cpp
+++ expiration_test.cpp
@@ -6,14 +6,15 @@ using namespace Patrick;
 
 int main()
 {
-  std::shared_ptr<Cache> c(new Cache);
+  std::shared_ptr<Cache> c(new Cache("Cut"));
   
   c->add("expires-4", "test", optional<int>(4));
   c->set("expires-2", "test2", optional<int>(2));
   std::cout << "get expires-4" << std::endl << *(c->get("expires-4")) << std::endl;
 
+  PeriodicExecutor<Cache> p(1, c, &Cache::cleanUp);
   std::cout << "Cache started" << std::endl;
-  PeriodicExecutor<std::shared_ptr<Cache>> p(1, c, &Cache::cleanUp);
-
-  // while(true);
+  
+  std::cout << "get expires-4" << std::endl << *(c->get("expires-4")) << std::endl;
+  while(true);
 }
--- periodic_executor.h
+++ periodic_executor.h
@@ -10,17 +10,18 @@ namespace Patrick {
   template<class T>
   struct PeriodicExecutor
   {
-    PeriodicExecutor(int timer, T& obj, std::function<void(T&)> f) {
+    PeriodicExecutor(int timer, std::shared_ptr<T>& obj, std::function<void(T&)> f) {
       start(timer, obj, f);
     }
 
-    void start(int timer, T& obj, std::function<void(T&)> f) {
-      while(true) {
-        auto result = std::async(std::launch::async, [timer, &obj, &f] {
-            std::this_thread::sleep_for(std::chrono::seconds( timer ));
-            f(obj);
-        });
-      }
+    void start(int timer, std::shared_ptr<T>& obj, std::function<void(T&)> f) {
+      std::thread t([=] {
+        while(true) {
+          std::this_thread::sleep_for(std::chrono::seconds( timer ));
+          f(*obj);
+        }
+      });
+      t.detach();
     }
   };
 }
